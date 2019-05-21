a841057ecdecd58d2130ffd5005fb9bc1fc3b7ec
gabrielmandelli <gabriel-mandelli@hotmail.com>
2019-05-21 01:24:11 -0300

    ajust salesman

--- src/main/java/org/start/baseApi/controller/SalesmanController.java
+++ src/main/java/org/start/baseApi/controller/SalesmanController.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("salesman")
 public class SalesmanController {
 
     @Autowired
